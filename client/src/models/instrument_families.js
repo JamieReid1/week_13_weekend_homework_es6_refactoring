9487a1804b35cf99d5f492a7f235eb3d1f86fbcc
Jamie Reid <wjreid85.jr@gmail.com>
2019-03-01 14:14:01 +0000

    Classes and imports/exports refactored.

---
+++ client/src/models/instrument_families.js
@@ -0,0 +1,27 @@
+import PubSub from '../helpers/pub_sub.js';
+
+class InstrumentFamilies {
+// const InstrumentFamilies = function (data) {
+  constructor(data){
+    this.data = data;
+  };
+
+// InstrumentFamilies.prototype.bindEvents = function () {
+  bindEvents(){
+    PubSub.publish('InstrumentFamilies:data-ready', this.data);
+
+    PubSub.subscribe('SelectView:change', (evt) => {
+      console.log(evt.detail);
+      const selectedIndex = evt.detail;
+      this.publishFamilyDetail(selectedIndex);
+    });
+  };
+
+// InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
+  publishFamilyDetail(selectedIndex){
+    const selectedFamily = this.data[selectedIndex];
+    PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+  };
+
+}
+export default InstrumentFamilies;
